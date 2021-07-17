a2e9c2c25b7d6ec50e9a760558e8ea9e2ab78e61
yichuan~ <73766326+yichuan520030910320@users.noreply.github.com>
2021-07-17 10:29:33 +0800

    book2 image8

--- raytracer/src/perlin.rs
+++ raytracer/src/perlin.rs
@@ -62,10 +62,43 @@ impl Perlin {
         }
     }
     pub fn noise(&self, p: Vec3) -> f64 {
-        let i = ((4.0 * p.x) as i32) & 255;
-        let j = ((4.0 * p.y) as i32) & 255;
-        let k = ((4.0 * p.z) as i32) & 255;
-        return self.ranfloat[(self.perm_x[i as usize] ^ self.perm_y[j as usize] ^ self.perm_z[k as usize]) as usize];
+        let u = p.x - p.x.floor();
+        let v = p.y - p.y.floor();
+        let w = p.z - p.z.floor();
+        let i = (p.x.floor()) as i32;
+        let j = (p.y.floor()) as i32;
+        let k = (p.z.floor()) as i32;
+        let mut my_sz = [[[0.0; 2]; 2]; 2];
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    my_sz[di][dj][dk] = self.ranfloat[
+                       ( (self.perm_x[((i + (di as i32)) & 255)as usize] as usize)^
+                           ( self.perm_y[((j + (dj as i32)) & 255)as usize]as usize )^
+                            (self.perm_z[((k + (dk as i32)) & 255)as usize]as usize))as usize
+                        ];
+                }
+            }
+        }
+        return Perlin::trilinear_interp(my_sz,u,v,w);
+
+
+        // let i = ((4.0 * p.x) as i32) & 255;
+        // let j = ((4.0 * p.y) as i32) & 255;
+        // let k = ((4.0 * p.z) as i32) & 255;
+        // return self.ranfloat[(self.perm_x[i as usize] ^ self.perm_y[j as usize] ^ self.perm_z[k as usize]) as usize];
+    }
+
+    pub fn trilinear_interp(mut my_sz: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    accum += ((i as f64) * u + (((1 - i))as f64) * (1.0 - u)) * ((j as f64* v) + ((1 - j)as f64) * (1.0 - v) )* ((k as f64)* w + (((1 - k))as f64) * (1.0- w)) * my_sz[i][j][k];
+                }
+            }
+        }
+        return accum;
     }
 }
 
@@ -76,7 +109,7 @@ pub struct NoiseTexture {
 impl NoiseTexture {
     pub fn new() -> Self {
         Self {
-            noise:Perlin::new()
+            noise: Perlin::new()
         }
     }
 }
